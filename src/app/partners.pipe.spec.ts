184db34672a5eb51713c3157063e8041ca70df7d
Gunses1987 <62013389+Gunses1987@users.noreply.github.com>
2021-10-04 20:50:12 +0400

    da

--- src/app/partners.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PartnersPipe } from './partners.pipe';
-
-describe('PartnersPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PartnersPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
